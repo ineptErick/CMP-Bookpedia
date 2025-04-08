39525d2485c41c1e47910577a935c9bf1d59169f
ineptErick <inepterick@gmail.com>
2025-04-08 13:04:15 +0300

    Bookpedia app first push

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -1,20 +1,7 @@
 package com.plcoding.bookpedia
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import cmp_bookpedia.composeapp.generated.resources.Res
-import cmp_bookpedia.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
