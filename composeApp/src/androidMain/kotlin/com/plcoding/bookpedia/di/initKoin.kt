7871be83cd20befd1513c6b89b47dda0abb73b17
ineptErick <inepterick@gmail.com>
2025-05-10 14:32:26 +0300

    study comments added

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/di/initKoin.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/di/initKoin.kt
@@ -3,9 +3,18 @@ package com.plcoding.bookpedia.di
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 
+// инициализации Koin — библиотеки для внедрения зависимостей в приложениях на Kotlin
+// принимает один параметр config
+// Тип параметра — KoinAppDeclaration?
+// значение по умолчанию null, позволяет вызывать функцию без передачи аргумента
 fun initKoin(config: KoinAppDeclaration? = null){
-    startKoin{
-        config?.invoke(this)
-        modules(sharedModule, platformModule)
+    startKoin{ // инициализация Koin
+        // config не равен null, вызывается метод invoke на объекте config, передавая в него текущий контекст Koin
+        // позволяет пользователю передать свою собственную конфигурацию для настройки Koin, если он этого хочет
+        config?.invoke(this) //  (?.) для проверки, не является ли config равным null
+
+        modules(sharedModule, platformModule) // добавляет модули в Koin
+        // sharedModule и platformModule — переменные, которые должны содержать определения модулей Koin
+    // различные зависимости и их реализации, которые Koin будет управлять
     }
 }
--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/Modules.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/di/Modules.kt
@@ -16,21 +16,42 @@ import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
-expect val platformModule: Module
+// определяет модули Koin, которые содержат различные зависимости,
+// используемые в приложении
 
+// expect: ключевое слово в Kotlin Multiplatform
+// для объявления ожидаемого значения или класса,
+// которое должно быть реализовано в конкретной платформе
+expect val platformModule: Module // реализация этой переменной (platformModule) будет предоставлена в специфичных для платформы файлах
+
+// модуль Koin, который может содержать определения зависимостей
 val sharedModule = module {
-    single { HttpClientFactory.create(get()) }
+    // регистрирует зависимость как синглтон - при каждом запросе этой зависимости будет возвращаться один и тот же экземпляр
+    single { HttpClientFactory.create(get()) } // создается экземпляр HTTP-клиента с помощью фабрики HttpClientFactory
+    // регистрирует синглтон для класса KtorRemoteBookDataSource
+    //  :: используется для ссылки на конструктор класса
+    // связываем зарегистрированный класс KtorRemoteBookDataSource с интерфейсом RemoteBookDataSource
+    // позволяет Koin возвращать экземпляр KtorRemoteBookDataSource, когда запрашивается RemoteBookDataSource
     singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
+    // при запросе RemoteBookDataSource будет возвращен экземпляр DefaultBookRepository
     singleOf(::DefaultBookRepository).bind<RemoteBookDataSource>()
 
     single {
-        get<DatabaseFactory>().create()
-            .setDriver(BundledSQLiteDriver())
-            .build()
-    }
+        get<DatabaseFactory>().create() // создать новый экземпляр базы данных
+            .setDriver(BundledSQLiteDriver()) // драйвер базы данных
+            .build() // Завершаем создание базы данных
+    } // В результате будет создана база данных, которая будет храниться как синглтон.
+
+    // Получаем DAO (Data Access Object) для работы с любимыми книгами из базы данных.
     single { get<FavoriteBookDatabase>().favoriteBookDao }
 
+    // Регистрирует ViewModel для BookListViewModel
+    // Koin будет управлять жизненным циклом этой ViewModel и предоставлять ее экземпляр по запросу
     viewModelOf(::BookListViewModel)
     viewModelOf(::BookDetailViewModel)
     viewModelOf(::SelectedBookViewModel)
+
+    // В результате этого кода создается модуль Koin,
+// который содержит определения различных зависимостей и ViewModel'ей,
+// используемых в приложении.
 }
