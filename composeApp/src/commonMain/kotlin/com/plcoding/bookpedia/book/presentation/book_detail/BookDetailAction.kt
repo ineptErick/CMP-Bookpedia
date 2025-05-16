7c0bf3d1d6a476393a475130ae7565b7246ef849
ineptErick <inepterick@gmail.com>
2025-05-16 15:58:53 +0300

    BookDetailViewModel created + small updates

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailAction.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailAction.kt
@@ -2,10 +2,14 @@ package com.plcoding.bookpedia.book.presentation.book_detail
 
 import com.plcoding.bookpedia.book.domain.Book
 
+// определения различных действий,
+// которые могут произойти в контексте деталей книги в приложении
+
+// все возможные реализации должны находиться в одном файле
 sealed interface BookDetailAction {
 
-    data object onBackClick: BookDetailAction
-    data object onFavoriteClick: BookDetailAction
-    data class OnSelectedBookChange(val book: Book): BookDetailAction
+    data object onBackClick: BookDetailAction // действие возврата назад, реализует интерфейс
+    data object onFavoriteClick: BookDetailAction // добавления или удаления книги из избранного
+    data class OnSelectedBookChange(val book: Book): BookDetailAction // изменения выбранной книги с передачей объекта книги
 
 }
