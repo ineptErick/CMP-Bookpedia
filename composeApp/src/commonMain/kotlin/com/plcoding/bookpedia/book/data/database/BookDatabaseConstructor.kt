855f38c44a1c22d0a0100728b3f7954eeff05f8e
ineptErick <inepterick@gmail.com>
2025-05-22 14:14:04 +0300

    DatabaseFactory corrected

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/BookDatabaseConstructor.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/database/BookDatabaseConstructor.kt
@@ -2,7 +2,7 @@ package com.plcoding.bookpedia.book.data.database
 
 import androidx.room.RoomDatabaseConstructor
 
-@Suppress("NO_ACTUAL_FOR_EXPERT")
+@Suppress("NO_ACTUAL_FOR_EXPECT")
 expect object BookDatabaseConstructor: RoomDatabaseConstructor<FavoriteBookDatabase> {
     override fun initialize(): FavoriteBookDatabase
 }
